cf22e64e50e4a968c7f2d85989cc5f0eb5d42d1c
pardeep bhasin <pardeep.bhasin@nagarro.com>
2022-08-18 01:32:20 +0530

    test case code changes

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -4,14 +4,12 @@ import React from "react";
 import { render, screen } from "../test-utils";
 import HomePage from "./index";
 
-describe("HomePage", () => {
-  it("should render the heading", () => {
-    render(<HomePage />);
+it("should render the heading", () => {
+  render(<HomePage />);
 
-    const heading = screen.getByText("Hello World");
+  const heading = screen.getByText("Hello World");
 
-    // we can only use toBeInTheDocument because it was imported
-    // in the jest.setup.js and configured in jest.config.js
-    expect(heading).toBeInTheDocument();
-  });
+  // we can only use toBeInTheDocument because it was imported
+  // in the jest.setup.js and configured in jest.config.js
+  expect(heading).toBeInTheDocument();
 });
