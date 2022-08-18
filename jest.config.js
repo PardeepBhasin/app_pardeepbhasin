fa976c6dbea80aa8b8f4696d5a28d0d002098be6
pardeep bhasin <pardeep.bhasin@nagarro.com>
2022-08-18 10:01:29 +0530

    updated

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     injectGlobals: true,
     setupFilesAfterEnv: ["./jest.setup.js"],
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/','<rootDir>/*.js/'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     moduleNameMapper: {
         '\\.(scss|sass|css)$': 'identity-obj-proxy',
     },
