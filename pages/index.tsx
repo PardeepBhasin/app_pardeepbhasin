b4726f5e739c6ae8d2c776bb2f610a7a87dee40a
pardeep bhasin <pardeep.bhasin@nagarro.com>
2022-08-20 20:36:52 +0530

    updated code

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World
+          Hello World Master
         </h1>
       </main>
     </div>
